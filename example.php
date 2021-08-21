5c1d6a1f697f423194cae436476e43378ee5a14e
codeeater_ <55358751+toxichead@users.noreply.github.com>
2021-08-21 04:38:04 +0300

    2 much updates

--- example.php
+++ example.php
@@ -1,57 +1,180 @@
-<?
+<?php
 include 'tgLib.php';
 
-const TOKEN = 'my token is here!';
+const TOKEN = '995442052:AAHRIMxoAPt_pAc2XD_1rjU3LM8Tv3bVsKw';
 $bot = new tgBot(TOKEN);
 $data = json_decode(file_get_contents('php://input'),true);
-if (!$data) die;
-$text = $data['message']['text'];
-$chat = $data['message']['chat']['id'];
-$id = $data['message']['from']['id'];
-$first_name = $data['message']['from']['first_name'];
-if ($data['message']['reply_to_message']['from']['id']) {$reply_author = $data['message']['reply_to_message']['from']['id'];}
-if ($data['message']['reply_to_message']['message_id']) {$reply_message_id = $data['message']['reply_to_message']['message_id'];}
+if (!$data) die('ok');
 
-if ($text){
-    if (strtolower($text) == "/start") $bot->reply($chat, "*Hello.*\nI'm Telegram bot!.");
-    if (strtolower($text) == "/invitelink") $bot->reply($chat,"*Chat's link.*\n{$bot->chatInviteLink($chat)}");
-    if (strtolower($text) == "/kick") {
-    	if ($reply_author){
-    		$bot->kick($chat,$reply_author);
-    		$bot->reply($chat,"[User](tg://user?id={$reply_author}) kicked, [{$first_name}](tg://user?id={$id}).");
-    	} else {
-    		$bot->reply($chat, "Reply on message and write this command.");
-    	}
-    } 
-    if(strtolower($text) == "/picture") $bot->pictureReply($chat,"*It's a picture*, [{$first_name}](tg://user?id={$id})!", "http://pm1.narvii.com/7360/5109a4d976422ebdcdbfa23563d6b37a10a27966r1-736-1104v2_hq.jpg");
-    if(strtolower($text) == "/video") $bot->videoReply($chat,"*It's a video*, [{$first_name}](tg://user?id={$id}).", "link to video or id");
-    if(strtolower($text) == "/gif") $bot->gifReply($chat,"*It's a GIF*, [{$first_name}](tg://user?id={$id}).", "https://media.giphy.com/media/ifAxq0ON2i5fcjeOdR/giphy.gif");
-    if(strtolower($text) == "/audio") $bot->audioReply($chat,"*It's a music*, [{$first_name}](tg://user?id={$id}).", "link to mp3 or id");
-    if(strtolower($text) == "/voice") $bot->voiceReply($chat,"*It's a voice*, [{$first_name}](tg://user?id={$id}).", "link to opus ogg or id"); //ONLY .ogg IN OPUS CODEC!!!!
-    if(strtolower($text) == "/videonote") $bot->videoNoteReply($chat, "link to videonote or id");
-    if(strtolower(mb_substr($text,0,9)) == "/newtitle") {
-        $bot->setChatTitle($chat,mb_substr($text,10));
-        $bot->reply($chat,"*Name of chat changed*, [{$first_name}](tg://user?id={$id}).");
-    }
-    if(strtolower($text) == "/pin") {
-        if (!$reply_message_id) die($bot->reply($chat,"Reply on message with this command."));
-        $bot->pinMessage($chat,$reply_message_id);
-        $bot->reply($chat,"Pinned, [{$first_name}](tg://user?id={$id}).");
-    }
-    if(strtolower($text) == "/unpin") {
-        $bot->unpinMessage($chat);
-        $bot->reply($chat,"Unpinned, [{$first_name}](tg://user?id={$id}).");
-        }
-    if(strtolower(mb_substr($text,0,8)) == "/tempban") {
-        if ($reply_author){
-            $unixtime = $bot->toUnix(mb_substr($text,mb_stripos($text," ")));
-            $bot->reply($chat,$unixtime);
-    		$tempban = $bot->tempban($chat,$reply_author,$unixtime);
-    		$bot->reply($chat,"[User](tg://user?id={$reply_author}) banned for a {$unixtime} seconds, [{$first_name}](tg://user?id={$id})."); //if time<30 or >1 year - ban for forever
-    	} else {
-    		$bot->reply($chat, "Reply on message with this command.");
-    	}
-    }
-    
-} 
-exit('ok'); //tg can be shocked if willn't see 'ok'))))
+if (!$data['callback_query']) {
+  $text = $data['message']['text'];
+  $textArr = explode (' ', $text); //for finding commands and arguments f.e.
+  $chat = $data['message']['chat']['id'];
+  $id = $data['message']['from']['id'];
+  $first_name = $data['message']['from']['first_name'];
+  if ($data['message']['reply_to_message']['from']['id']) {$reply_author = $data['message']['reply_to_message']['from']['id'];}
+  if ($data['message']['reply_to_message']['message_id']) {$reply_message_id = $data['message']['reply_to_message']['message_id'];}
+}
+if ($text != null && isset($text))
+  switch ($textArr[0]) //better than if-else conditions
+  {
+    case '/start':
+      $bot->reply($chat, "*Hello!*\nI'm Telegram bot.");
+      break;
+    case '/invitelink':
+      $link = $bot->chatInviteLink($chat);
+      if (!$link)
+        $bot->reply($chat, "*Chat's link is unavailable here.*");
+      else
+        $bot->reply($chat,"*Chat's link.*\n{$link}");
+      break;
+    case '/kick':
+      if ($reply_author){
+        $bot->kick($chat,$reply_author);
+        $bot->reply($chat,"[User](tg://user?id={$reply_author}) kicked, [{$first_name}](tg://user?id={$id}).");
+      } else
+          $bot->reply($chat, "Reply on message and write this command.");
+      break;
+    case '/picture':
+      $bot->pictureReply($chat,"*It's a picture*, [{$first_name}](tg://user?id={$id})!", "http://pm1.narvii.com/7360/5109a4d976422ebdcdbfa23563d6b37a10a27966r1-736-1104v2_hq.jpg");
+      break;
+    case '/video':
+      $bot->videoReply($chat,"*It's a video*, [{$first_name}](tg://user?id={$id}).", "link to video or id");
+      break;
+    case '/gif':
+      $bot->gifReply($chat,"*It's a GIF*, [{$first_name}](tg://user?id={$id}).", "https://media.giphy.com/media/ifAxq0ON2i5fcjeOdR/giphy.gif");
+      break;
+    case '/audio':
+      $bot->audioReply($chat,"*It's a music*, [{$first_name}](tg://user?id={$id}).", "link to mp3 or id");
+      break;
+    case '/voice':
+      $bot->voiceReply($chat,"*It's a voice*, [{$first_name}](tg://user?id={$id}).", "link to opus ogg or id"); //ONLY .ogg IN OPUS CODEC!!!!
+      break;
+    case '/videonote':
+      $bot->videoNoteReply($chat, "link to videonote or id");
+      break;
+    case '/newtitle':
+      $bot->setChatTitle($chat,mb_substr($text,10));
+      $bot->reply($chat,"*Name of chat changed*, [{$first_name}](tg://user?id={$id}).");
+      break;
+    case '/pin':
+      if (!$reply_message_id)
+      {
+        $bot->reply($chat,"Reply on message with this command.");
+        exit;
+      }
+      $bot->pinMessage($chat,$reply_message_id);
+      $bot->reply($chat,"Pinned, [{$first_name}](tg://user?id={$id}).");
+      break;
+    case '/unpin':
+      $bot->unpinMessage($chat);
+      $bot->reply($chat,"Unpinned, [{$first_name}](tg://user?id={$id}).");
+      break;
+    case '/tempban':
+      if ($reply_author)
+      {
+        $unixtime = $bot->toUnix($textArr[1]); //ban period
+        $bantime = time() + $unixtime; //date when person will be unbanned
+        $tempban = $bot->tempban($chat,$reply_author,$unixtime);
+        $bot->reply($chat,"[User](tg://user?id={$reply_author}) banned for a {$unixtime} seconds, [{$first_name}](tg://user?id={$id})."); //if $unixtime < 30 or $unixtime > 1 year - ban for forever
+      } else
+        $bot->reply($chat, "Reply on message with this command.");
+      break;
+    case '/time': //time converter lmao))
+      if (!$textArr[1])
+      {
+        $bot->reply($chat, "Can't see your timestamp.");
+        exit;
+      }
+        $answer = $bot->toUnix($textArr[1]);
+      if ($answer['error_code'])
+        $bot->reply($chat, "{$answer['error_code']}: {$answer['description']}.");
+      else
+        $bot->reply($chat, "In UNIX it will be `{$answer}`.");
+      break;
+    case '/openkeyboard':
+      $rawButtons =
+      [
+        [
+          "Button1",
+          "Button2",
+          "Button3"
+        ],
+        [
+          'Hide this'
+        ]
+      ];
+      $buttons = $bot->keyboardMakeup('keyboard', $rawButtons, ['resize_keyboard' => true, "one_time_keyboard" => false]); //array is not required, i just show examples of additional params
+      $bot->reply($chat, "**Click the buttons!**", $buttons);
+      break;
+    //answers on keyboard's buttons (IT cAN cONFUSE YOU AND YOUR cODE!! BEWARE!! ANSWERS FROM KEYBOARD'S BUTTONS ARE THE SAME LIKE FROM USUAL TEXT!!!)
+    case 'Button1':
+    case 'Button2':
+    case 'Button3':
+      $bot->reply($chat, "You pressed the button!");
+      break;
+    case 'Hide':
+      if ($textArr[1] == 'this' || $textArr[1] == 'keyboard')
+        $bot->reply($chat, "Keyboard is hidden now.", $bot->hideKeyboard());
+      break;
+    //
+    case '/inlinebuttons':
+      $rawButtons =
+      [
+        [
+          $bot->inlineButton("First Button", "Button1"),
+          $bot->inlineButton("Second Button", "Button2")
+        ],
+        [
+          $bot->inlineButton("Third Button", "Button3")
+        ],
+        [
+          $bot->inlineButton("Test 1", "cbTest1"),
+          $bot->inlineButton("Test 2", "cbTest2"),
+          $bot->inlineButton("Test 3", "cbTest3")
+        ]
+      ];
+      $buttons = $bot->keyboardMakeup ('inline_keyboard', $rawButtons);
+      $bot->reply($chat,"**Click the buttons!**", $buttons);
+      break;
+    default:
+      exit('ok');
+      break;
+  }
+if ($data['callback_query'])
+{
+  $button_callback = $data['callback_query']['data'];
+  $chat = $data['callback_query']['message']['chat']['id'];
+  $id = $data['callback_query']['from']['id'];
+  $first_name = $data['callback_query']['from']['first_name'];
+  $message_id = $data['callback_query']['message']['message_id']; //maybe for someone it will be useful
+  $cbqID = $data['callback_query']['id'];
+  switch ($button_callback) {
+    case 'Button1':
+      $bot->reply($chat, "You touched first inline button!");
+      $bot->endCB($cbqID); //it's shorted 'callbackAnswer' function just for disable loading circle
+      break;
+    case 'Button2':
+      $bot->reply($chat, "You touched second inline button!");
+      $bot->endCB($cbqID);
+      break;
+    case 'Button3':
+      $bot->reply($chat, "You touched third inline button!");
+      $bot->endCB($cbqID);
+      break;
+    case 'cbTest1':
+      $bot->callbackAnswer($cbqID, "This is test!");
+      break;
+    case 'cbTest2':
+      $bot->callbackAnswer($cbqID, "This is test!", ['show_alert' => true]);
+      break;
+    case 'cbTest3':
+      $bot->callbackAnswer($cbqID, "a.", ['url' => 'https://github.com/toxichead/telelibonphp']);
+      break;
+    default:
+      exit('ok');
+      break;
+  }
+}
+
+exit('ok');
--- tgLib.php
+++ tgLib.php
@@ -6,66 +6,136 @@ class tgBot{
     public function __construct($token){
         $this->token = $token;
     }
-    
-    public function request($method, $params = []){ //да-да, request на post-е. в коем-то веке.
+
+    public function request($method, $params = []){
         $url = 'https://api.telegram.org/bot' . $this->token .  '/' . $method;
         $curl = curl_init();
-          
+
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $params); 
-          
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+
         $out = json_decode(curl_exec($curl), true);
-          
-        curl_close($curl); 
-          
-        return $out; 
+
+        curl_close($curl);
+
+        return $out;
     }
-    
-    public function reply($chat,$text){
-        $a = $this->request('sendMessage', ["parse_mode" => "markdown", "chat_id" => $chat, "text" => $text]);
-        return $a;
+    //KEYBOARD SHIT
+    public function keyboardMakeup ($type, $buttonsArray, $additional_params = null)
+    {
+      $answer = [$type => $buttonsArray];
+      if ($additional_params != null)
+        $answer = array_merge($answer, $additional_params);
+      return $answer;
+    }
+
+    public function inlineButton($text, $callback_query, $additional_params = null)
+    {
+      $answer = ['text' => $text, "callback_data" => $callback_query];
+      if ($additional_params != null)
+        $answer = array_merge($answer, $additional_params);
+      return $answer;
+    }
+
+    public function callbackAnswer ($callback_query_id, $text = null, $additional_params = null)
+    {
+      $params = ['callback_query_id' => $callback_query_id];
+      if ($text != null)
+        $params['text'] = $text;
+      if ($additional_params != null)
+        $params = array_merge ($params, $additional_params);
+      $a = $this->request('answerCallbackQuery', $params);
+      return $a;
+    }
+    public function endCB ($id) //fast alias to remove loading circle on inline button
+    {
+      return $this->callbackAnswer($id);
+    }
+    public function hideKeyboard () //fast alias to hide keyboard
+    {
+      return ['remove_keyboard' => true];
+    }
+    //ended, now other
+    public function reply($chat,$text, $keyboard = null, $additional_params = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "text" => $text];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        if ($additional_params != null)
+          $params = array_merge($params,$additional_params);
+        return $this->request('sendMessage', $params);
     }
     public function kick($chat,$userid){
-        $a = $this->request('kickChatMember', ["chat_id" => $chat, 'user_id' => $userid]); return $a;
+        return $this->request('kickChatMember', ["chat_id" => $chat, 'user_id' => $userid]);
     }
     public function createPoll($chat,$question,$answers = []){
-        $a = $this->request('sendPoll', ["chat_id" => $chat, 'question' => $question, "options" => $answers]); return $a;
+        $a = $this->request('sendPoll', ["chat_id" => $chat, 'question' => $question, "options" => $answers]);
+        return $a;
     }
-    public function pictureReply($chat,$text,$url_of_picture){
-        $a = $this->request('sendPhoto', ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "photo" => $url_of_picture]); return $a;
+    public function pictureReply($chat,$text,$url_of_picture, $keyboard = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "photo" => $url_of_picture];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $a = $this->request('sendPhoto', $params);
+        return $a;
     }
-    public function videoReply($chat,$text,$url_of_video){
-        $a = $this->request('sendVideo', ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "video" => $url_of_video]); return $a;
+    public function videoReply($chat,$text,$url_of_video, $keyboard = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "video" => $url_of_video];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $a = $this->request('sendVideo', $params); return $a;
     }
-    public function gifReply($chat,$text,$url_of_gif){
-        $a = $this->request('sendAnimation', ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "animation" => $url_of_gif]); return $a;
+    public function gifReply($chat,$text,$url_of_gif, $keyboard = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "animation" => $url_of_gif];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        //request
+        $a = $this->request('sendAnimation', $params);
+        return $a;
     }
-    public function audioReply($chat,$text,$url_of_audio){
-        $a = $this->request('sendAudio', ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "audio" => $url_of_audio]);return $a;
+    public function audioReply($chat,$text,$url_of_audio, $keyboard = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "audio" => $url_of_audio];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $a = $this->request('sendAudio', $params);
+        return $a;
     }
-    public function voiceReply($chat,$text,$url_of_voice){
-        $a = $this->request('sendVoice', ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "voice" => $url_of_voice]);return $a;
+    public function voiceReply($chat,$text,$url_of_voice, $keyboard = null){
+        $params = ["parse_mode" => "markdown", "chat_id" => $chat, "caption" => $text, "voice" => $url_of_voice];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $a = $this->request('sendVoice', $params);
+        return $a;
     }
-    public function videoNoteReply($chat,$url_of_vidnote){
-        $a = $this->request('sendVideoNote', ["chat_id" => $chat, "video" => $url_of_vidnote]);return $a;
+    public function videoNoteReply($chat,$url_of_vidnote, $keyboard = null){ //URL UNSUPPORTED
+        $params = ["chat_id" => $chat, "video" => $url_of_vidnote];
+        if ($keyboard != null)
+          $params['reply_markup'] = json_encode($keyboard, JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $a = $this->request('sendVideoNote', $params);
+        return $a;
     }
     public function setChatTitle($chat,$title){
-        $a = $this->request('setChatTitle', ["chat_id" => $chat, "title" => $title]);return $a;
+        $a = $this->request('setChatTitle', ["chat_id" => $chat, "title" => $title]);
+        return $a;
     }
     public function chatInviteLink($chat){
-        $a = $this->request('exportChatInviteLink', ["chat_id" => $chat]);return $a['result'];
+        $a = $this->request('exportChatInviteLink', ["chat_id" => $chat]);
+        return $a['result'];
     }
     public function pinMessage($chat,$message_id){
-        $a = $this->request('pinChatMessage', ["chat_id" => $chat, "message_id" => $message_id]);return $a;
+        $a = $this->request('pinChatMessage', ["chat_id" => $chat, "message_id" => $message_id]);
+        return $a;
     }
     public function unpinMessage($chat){
-        $a = $this->request('unpinChatMessage', ["chat_id" => $chat]);return $a;
+        $a = $this->request('unpinChatMessage', ["chat_id" => $chat]);
+        return $a;
     }
     public function tempban($chat,$userid,$time){
-        $a = $this->request('kickChatMember', ["chat_id" => $chat, "user_id" => $userid, "until_date" => $this->toUnix($time)]);return $a;
+        $a = $this->request('kickChatMember', ["chat_id" => $chat, "user_id" => $userid, "until_date" => $time]);
+        return $a;
     }
     //))
     public function toUnix($mainstr){
@@ -75,8 +145,9 @@ class tgBot{
     		return $time;
 	    	}
     	elseif (mb_stripos($mainstr, 's')) {
-    		$array = ["error_code" => "FALSE_TIME", "description" => "SECONDS ARE UNSUPPORTED"];
-    	    return $array;
+        $end = mb_stripos($mainstr, 's');
+        $timeTEMP = mb_substr($mainstr,0,$end);
+        return $timeTEMP; //in theory this shit works
     	}
     	elseif (mb_stripos($mainstr, 'h')) {
     		$end = mb_stripos($mainstr, 'h');
@@ -93,7 +164,7 @@ class tgBot{
     		$timeTEMP = mb_substr($mainstr,0,$end); $time = $timeTEMP * 604800;
     		return $time;
     		}
-    	elseif (mb_stripos($mainstr, 'mn')) {
+    	elseif (mb_stripos($mainstr, 'M')) {
 	    	$end = mb_stripos($mainstr, 'M');
 	    	$timeTEMP = mb_substr($mainstr,0,$end); $time = $timeTEMP * 2629743;
     		return $time;
@@ -107,5 +178,5 @@ class tgBot{
     	    $array = ["error_code" => "NO_TIME", "description" => "TIME ISN'T POINTED OR INCORRECT"];
     	    return $array;
     	}
-    }  
+    }
 }
